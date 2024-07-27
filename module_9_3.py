73141a47fde7bd16ac21680c9ea0f0eaf6a4d5d1
Sergeystar76 <kertex@yandex.ru>
2024-07-27 17:27:16 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,9 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+zp = zip(first,second)
+first_result = (len(x) - len(y) for x,y in zip(first,second) if len(x) != len(y))
+second_result = (len(first[i]) == len(second[i]) for i in range(3))
+
+print(list(first_result))
+print(list(second_result))
+
