a4cecc66729c902b08cc218f5935ae3dd991db11
Sergeystar76 <kertex@yandex.ru>
2024-07-27 20:20:57 +0300

    Add files via upload

--- module_9_4.py
+++ module_9_4.py
@@ -1,21 +1,22 @@
 import random
-
+# Lambda-функция:
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
-
 my_func = lambda x, y: x == y
 print(list(map(my_func, first, second)))
 
+# Замыкание:
 def get_advanced_writer(file_name):
     def write_everything(*data_set):
         with open(file_name, 'w', encoding='utf-8') as file:
             for i in data_set:
-                    file.write(str(i) + '\n')
+                file.write(str(i) + '\n')
     return write_everything
 
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
 
+# Метод __call__:
 class MysticBall:
     def __init__(self, *words):
         self.words = words
