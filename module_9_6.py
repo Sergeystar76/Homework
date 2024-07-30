0feb575677b58d692836e7f55ab26d9c7a728287
Sergeystar76 <kertex@yandex.ru>
2024-07-30 19:18:42 +0300

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,8 @@
+def all_variants(text):
+    for i in range(len(text)):
+        for y in range(len(text) - i):
+            yield text[y:y + i + 1]
+
+a = all_variants("abc")
+for i in a:
+    print(i)
