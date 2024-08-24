8e49421cf639bfe3b5a2ddab93e9423b7300f12e
Sergeystar76 <kertex@yandex.ru>
2024-08-24 09:19:27 +0300

    Add files via upload

---
+++ module_10_5.py
@@ -0,0 +1,27 @@
+import multiprocessing
+import datetime
+files = ['file 1.txt', 'file 2.txt', 'file 3.txt', 'file 4.txt']
+def read_info(name):
+    all_data = []
+    with open(name, 'r', encoding='utf-8') as file:
+        data = file.readline()
+        while data:
+            data = file.readline()
+            all_data.append(data)
+
+
+# start_1 = datetime.datetime.now()
+# for file in files:
+#     read_info(file)
+# end_1 = datetime.datetime.now()
+# print(f'{end_1 - start_1}(линейный)')
+
+
+if __name__ == '__main__':
+    with multiprocessing.Pool(processes=4) as pool:
+        start = datetime.datetime.now()
+        pool.map(read_info, files)
+    end = datetime.datetime.now()
+    print(f'{end - start} (многопроцессный)')
+
+
