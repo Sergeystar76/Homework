4fe70c89528021ea7104fe128af5916a1ea91250
Sergeystar76 <kertex@yandex.ru>
2024-09-07 19:22:59 +0300

    Add files via upload

--- module_12_1.py
+++ module_12_1.py
@@ -3,10 +3,23 @@ import runner
 
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
-        self.assertEqual(runner.p1.distance, 50)
+        p1 = runner.Runner('Sergey')
+        for i in range(10):
+            p1.walk()
+        self.assertEqual(p1.distance, 50)
+
     def test_run(self):
-        self.assertEqual(runner.p2.distance, 100)
+        p2 = runner.Runner('Max')
+        for i in range(10):
+            p2.run()
+        self.assertEqual(p2.distance, 100)
     def test_challenge(self):
-            self.assertNotEqual(runner.p2.distance, runner.p1.distance)
+        p3 = runner.Runner('Anna')
+        p4 = runner.Runner('Olya')
+        for i in range(10):
+            p3.run()
+            p4.walk()
+
+            self.assertNotEqual(p3.distance, p4.distance)
 if __name__ == "__main__":
     unittest.main()
