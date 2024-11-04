a8acd4546c5bd4b1ab85f77f71b1ac9d4e14aa17
Sergeystar76 <kertex@yandex.ru>
2024-11-04 11:29:53 +0300

    Add files via upload

--- Module_17/app/routers/task.py
+++ Module_17/app/routers/task.py
@@ -1,18 +1,40 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
+# Сессия БД
+from sqlalchemy.orm import Session
+# Функция подключения к БД
+from app.backend.db_depends import get_db
+# Аннотации, Модели БД и Pydantic.
+from typing import Annotated
+from app.models import Task
+from app.schemas import CreateTask, UpdateTask
+from sqlalchemy import insert, select, update, delete
+# Функция создания slug-строки
+from slugify import slugify
 
 router = APIRouter(prefix="/task", tags=["task"])
 
 @router.get("/")
-async def all_taks():
-    pass
+async def all_taks(db: Annotated[Session, Depends(get_db)]):
+    tasks = db.scalars(select(Task)).all()
+    return tasks
 
 @router.get("/task_id")
-async def task_by_id():
-    pass
+async def task_by_id(db: Annotated[Session, Depends(get_db)], task_id: int):
+    task = db.scalar(select(Task).where(Task.id == task_id))
+    if task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User was not found')
+    return task
 
 @router.post("/create")
-async def create_task():
-    pass
+async def create_task(db: Annotated[Session, Depends(get_db)], create_task, user_id: CreateTask):
+    db.execute(insert(Task).values(title=create_task.title,
+                                   content=create_task.content,
+                                   priority=create_task.priority,
+                                   slug=slugify(create_task.title)))
+    db.commit()
+    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
 
 @router.put("/update")
 async def update_task():
